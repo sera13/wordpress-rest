f51679639d527523ca35a92534546439b40d7272
serafeim <serafeimkourlos@gmail.com>
2019-12-01 18:51:15 +0100

    Initial commit

---
+++ src/main/java/com/serafeim/agia/zoni/agiazoni/AgiazoniApplication.java
@@ -0,0 +1,13 @@
+package com.serafeim.agia.zoni.agiazoni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgiazoniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgiazoniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serafeim/agia/zoni/agiazoni/AgiazoniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serafeim.agia.zoni.agiazoni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgiazoniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
