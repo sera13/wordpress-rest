a758f25c75167c821b6f00450bb9cb4baf0c3198
Serafeim Kourlos <serafeimkourlos@gmail.com>
2022-01-07 19:19:00 +0200

    Revert the Hibernate for now

--- src/main/java/com/serafeim/agia/zoni/agiazoni/model/WPPost.java
+++ src/main/java/com/serafeim/agia/zoni/agiazoni/model/WPPost.java
@@ -1,19 +1,17 @@
 package com.serafeim.agia.zoni.agiazoni.model;
 
-import javax.persistence.*;
-
-@Entity(name = "wppost")
+//@Entity(name = "wppost")
 public class WPPost {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id", nullable = false)
-    private Long id;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    @Column(name = "id", nullable = false)
+//    private Long id;
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
 }
